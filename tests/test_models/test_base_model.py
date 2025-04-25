cd77bf4d12095cd2a3aebf8c35872e37acde4aed
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-25 19:52:46 +0100

    more unittesting

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,7 +4,7 @@
 """
 import unittest
 from models.base_model import BaseModel
-# from datetime import datetime
+from datetime import datetime
 import time
 
 
@@ -37,3 +37,29 @@ class TestBaseModel(unittest.TestCase):
     def test_str_representation_of_object(self):
         model = BaseModel()
         self.assertIsInstance(str(model), str)
+
+    def test_save_method_updates_updated_at(self):
+        """
+        Tests that the save() method updates the updated_at attribute.
+        """
+        bm = BaseModel()
+        # Get the initial updated_at time
+        initial_updated_at = bm.updated_at
+
+        # Introduce a small delay to ensure the datetime changes
+        time.sleep(0.01)
+
+        # Call the save method
+        bm.save()
+
+        # Get the updated updated_at time
+        updated_updated_at = bm.updated_at
+
+        # Assert that the updated_at time has changed
+        self.assertNotEqual(initial_updated_at, updated_updated_at)
+
+        # Assert that updated_updated_at is more recent than initial_updated_at
+        self.assertGreater(updated_updated_at, initial_updated_at)
+
+        # Assert that updated_updated_at is a datetime object
+        self.assertIsInstance(updated_updated_at, datetime)
