6f1d30eec4b69d12f22e2514b71a184a588bc3eb
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-26 11:53:36 +0100

    Implement a basic command interpreter

--- console.py
+++ console.py
@@ -1,1 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python5
+"""Defines the HBNBCommand class."""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB command line interpreter."""
+
+    def __init__(self):
+        """Initializes the interpreter."""
+        super().__init__()
+        self.prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quits the interpreter."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exits on Ctrl+D (EOF)."""
+        print()
+        return True
+
+    def do_help(self, arg):
+        """List available commands with "help" or detailed help with "help cmd"."""  # noqa
+        super().do_help(arg)
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
