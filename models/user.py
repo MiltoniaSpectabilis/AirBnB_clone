6e61b814191d8c36ce1da8bfa1ec5ed27ae5c30c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2025-04-29 15:15:44 +0100

    Comment out class constructor

--- models/user.py
+++ models/user.py
@@ -7,10 +7,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Represents a user in the system."""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes User instance."""
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    # def __init__( *args, **kwargs):
+    #     """Initializes User instance."""
+    #     super().__init__(*args, **kwargs)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
